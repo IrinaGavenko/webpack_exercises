a8ceb1cadd918404c6c82a19b1bc094bb07b5319
Irina Gavenko <irina-gavenko@9faf1387-f7a7-4787-a3a1-72ca0a640a26.attlocal.net>
2025-03-13 23:12:27 -0400

    watch mode: devServer

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,13 +1,17 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import type {Configuration as DevServerConfiguration} from 'webpack-dev-server';
 
 type Mode = 'development' | 'production';
 interface EnvVariables {
     mode: Mode,
+    port: number,
 }
 
 export default (env: EnvVariables) => {
+    const isDev = env.mode === 'development';
+
     const config: webpack.Configuration = {
         mode: env.mode ?? 'development',
         entry: path.resolve(__dirname, 'src', 'index.ts'),
@@ -19,8 +23,8 @@ export default (env: EnvVariables) => {
         plugins: [
             new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
             // show build progress, usually really slow
-            new webpack.ProgressPlugin(),
-        ],
+            isDev && new webpack.ProgressPlugin(),
+        ].filter(Boolean),
         module: {
             rules: [
                 {
@@ -33,6 +37,13 @@ export default (env: EnvVariables) => {
         resolve: {
             extensions: ['.tsx', '.ts', '.js'],
         },
+        // mapping between original code and generated scripts
+        devtool: isDev && 'inline-source-map',
+        devServer: isDev ? {
+            // npm run start -- --env port=5000
+            port: env.port ?? 3000,
+            open: true,
+        } : undefined,
     }
 
     return config;
