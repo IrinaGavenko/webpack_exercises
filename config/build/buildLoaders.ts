cc4339751bd5e4dbd63c5f0be48fe446c3bf8c39
Irina Gavenko <irina-gavenko@9faf1387-f7a7-4787-a3a1-72ca0a640a26.attlocal.net>
2025-03-21 23:00:01 -0400

    assets and svg

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -6,6 +6,37 @@ import {BuildOptions} from './types/types';
 export function buildLoaders({mode}: BuildOptions): ModuleOptions['rules'] {
     const isDev = mode === 'development';
 
+    const assetLoader = {
+        // without svg! Have special loader for svg
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+    };
+
+    const svgLoader = {
+        test: /\.svg$/,
+        // use: ['@svgr/webpack'],
+        // for using svg as icons - example: size changing
+        use: [
+            {
+                loader: '@svgr/webpack',
+                options: {
+                    icon: true,
+                    // for coloring whole svg
+                    svgoConfig: {
+                        plugins: [
+                            {
+                                name: 'convertColors',
+                                params: {
+                                    currentColor: true,
+                                }
+                            }
+                        ]
+                    }
+                },
+            },
+        ],
+    };
+
     const cssLoaderWithModules = {
         loader: 'css-loader',
         options: {
@@ -38,7 +69,9 @@ export function buildLoaders({mode}: BuildOptions): ModuleOptions['rules'] {
 
     // order in array is important!
     return [
+        assetLoader,
         sccsLoader,
         tsLoader,
+        svgLoader,
     ]
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,11 @@
 import {useState} from 'react';
 import {Outlet, Link} from 'react-router-dom';
 
+import birdPng from '@/assets/png_bird.png';
+import birdJpg from '@/assets/jpg_bird.jpg';
+import CocktailSvg from '@/assets/cocktail.svg';
+import IconSvg from '@/assets/icon.svg';
+
 import * as styles from './App.module.scss';
 
 
@@ -14,6 +19,25 @@ export const App = () => {
             <Link to={'/about'}>about</Link>
             <Link to={'shop'}>shop</Link>
             </div>
+
+            <div className={styles.pictureRoot}>
+                <img
+                    src={birdPng}
+                    alt="picture in png"
+                    className={styles.picture}
+                />
+
+                <CocktailSvg className={styles.picture} />
+
+                <img
+                    src={birdJpg}
+                    alt="picture in jpg"
+                    className={styles.picture}
+                />
+            </div>
+
+            {/** will brake CocktailSvg styles! **/}
+            <IconSvg width={20} height={20} className={styles.icon} />
             <div>{count}</div>
             <button className={styles.button} onClick={increment}>+</button>
             <Outlet />
