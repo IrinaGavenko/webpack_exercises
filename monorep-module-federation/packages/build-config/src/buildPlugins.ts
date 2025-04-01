72424971a66fecf1f538ea2a89c28d1da90f5f97
Irina Gavenko <irina-gavenko@9faf1387-f7a7-4787-a3a1-72ca0a640a26.attlocal.net>
2025-04-01 14:19:45 -0400

    bpackage.json and build-config form previous base exercise

---
+++ monorep-module-federation/packages/build-config/src/buildPlugins.ts
@@ -0,0 +1,54 @@
+import webpack, {Configuration, DefinePlugin} from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+
+import path from 'path';
+
+import {BuildOptions} from './types/types';
+
+export function buildPlugins({mode, paths, analyzer, platform}: BuildOptions): Configuration['plugins'] {
+    const isDev = mode === 'development';
+    const isProd = mode === 'production';
+
+    const localizationPath = path.resolve(paths.public, 'locales')
+
+    let plugins: Configuration['plugins'] = [
+        new HtmlWebpackPlugin({
+            template: paths.html,
+            favicon: path.resolve(paths.public, 'favicon.ico'),
+        }),
+        new DefinePlugin({
+            // all params that are necessary (dev, platform, etc.)
+            __PLATFORM__: JSON.stringify(platform),
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: localizationPath, to: path.resolve(paths.output, 'locales') },
+            ],
+        }),
+    ];
+
+    if (isDev) {
+        plugins.push(new webpack.ProgressPlugin())
+        // build still fast, adding strict type checking
+        plugins.push(new ForkTsCheckerWebpackPlugin())
+        plugins.push(new ReactRefreshWebpackPlugin())
+    }
+
+    if (isProd) {
+        plugins.push(new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }))
+    }
+
+    if (analyzer) {
+        plugins.push(new BundleAnalyzerPlugin())
+    }
+
+    return plugins;
+}
